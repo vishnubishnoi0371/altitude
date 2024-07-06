03cb71978889e8bffe4d33157306d96f78bd14b2
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-07-06 10:57:17 +0530

    gsap update

--- src/component/ProcessWork.jsx
+++ src/component/ProcessWork.jsx
@@ -9,23 +9,21 @@ const ProcessWork = () => {
         { image: '/assets/image/svg/inspect-icon.svg', content: 'Inspect property for storm damage or quote preparation.' },
         { image: '/assets/image/svg/meet-icon.svg', content: 'Meet with claims adjuster if applicable' },
         { image: '/assets/image/svg/planing-icon.svg', content: 'Production Planning (permitting/scheduling)' },
-        { image: '/assets/image/svg/construction-icon.svg', content: 'Estimate repair or replacement cost' },
+        { image: '/assets/image/svg/repair-icon.svg', content: 'Estimate repair or replacement cost' },
         { image: '/assets/image/svg/necessary-icon.svg', content: 'Finalize necessary documentation' },
-        { image: '/assets/image/svg/repair-icon.svg', content: 'Construction begins! (Most projects completed in 1 day!)' },
+        { image: '/assets/image/svg/construction-icon.svg', content: 'Construction begins! (Most projects completed in 1 day!)' },
     ];
 
-    const cardsRef = useRef([]);
     const circlesRef = useRef([]);
     const linesRef = useRef([]);
     const hrRef = useRef(null);
 
     useEffect(() => {
-        gsap.set(cardsRef.current, { opacity: 0, y: 50 });
         gsap.set(circlesRef.current, { boxShadow: '0px 0px 0px rgba(0, 0, 0, 0)' });
         gsap.set(linesRef.current, { opacity: 0 });
         gsap.set(hrRef.current, { scaleX: 0, transformOrigin: 'left center' });
 
-        const timeline = gsap.timeline({ repeat: -1, repeatDelay: 2 });
+        const timeline = gsap.timeline();
 
         steps.forEach((_, index) => {
             timeline
@@ -33,24 +31,23 @@ const ProcessWork = () => {
                     boxShadow: '0px 0px 20px rgba(255, 255, 255, 0.9)',
                     duration: 1,
                     ease: 'power1.inOut',
-                }, `+=${index === 0 ? 0 : 1}`) 
-                .to(cardsRef.current[index], {
-                    opacity: 1,
-                    y: 0,
-                    duration: 1,
-                    ease: 'power1.inOut',
-                    boxShadow: '0px 0px 20px rgba(255, 255, 255, 0.9)',
-                }, "<")
+                }, `+=${index === 0 ? 0 : 1}`)
+             
                 .to(hrRef.current, {
                     scaleX: 1,
                     duration: 1,
                     ease: 'power1.inOut',
-                }, "<") 
+                }, "<")
                 .to(linesRef.current[index], {
                     opacity: 1,
                     duration: 1,
                     ease: 'power1.inOut',
-                }, "<"); 
+                }, "<");
+        });
+
+        timeline.eventCallback("onComplete", () => {
+            gsap.to(circlesRef.current, { boxShadow: '0px 0px 0px rgba(0, 0, 0, 0)', duration: 0.5 });
+            gsap.to(cardsRef.current, { boxShadow: '0px 0px 0px rgba(0, 0, 0, 0)', duration: 0.5 });
         });
     }, []);
 
@@ -63,29 +60,28 @@ const ProcessWork = () => {
                         {steps.slice(0, 3).map((step, index) => (
                             <Customcard
                                 key={index}
-                                ref={el => cardsRef.current[index] = el}
                                 image={step.image}
                                 content={step.content}
                                 className={`animate-shadowMove delay-${index * 300}`}
                             />
                         ))}
                     </div>
-                    <div className='flex flex-col absolute max-md:top-[2%] left-0 sm:left-[47%] md:left-6 gap-32 sm:gap-[44px] md:gap-[80px] lg:gap-[120px] md:flex-row md:relative items-center max-md:justify-between'>
+                    <div className='flex flex-col absolute max-md:top-[2%] left-0 sm:left-[47%] md:left-3 gap-32 sm:gap-[44px] md:gap-[80px] lg:gap-[120px] md:flex-row md:relative items-center max-md:justify-between'>
                         {[...Array(6)].map((_, index) => (
                             <div key={index} className={`relative animate-shadowMove delay-${index * 300}`}>
                                 <p ref={el => circlesRef.current[index] = el} className='text-white relative text-xl md:text-2xl z-20 font-semibold leading-125 text-center flex items-center justify-center bg-secondary-blue mb-0 rounded-full w-[42px] h-[42px] md:w-[54px] md:h-[54px]'>{index + 1}</p>
-                                <span ref={el => linesRef.current[index] = el} className={`absolute rotate-90 md:rotate-0 z-10 top-0 width-[6px] height-[35px] xxs:height-[55px] ${index % 2 === 0 ? 'md:top-[-100%]' : 'md:top-[100%]'} max-md:${index % 2 === 0 ? '-rotate-90' : 'rotate-90'} left-[120%] md:left-[44%] animate-lineMove ${index === 0 || index === 2 || index === 4 ? 'sm:-rotate-90 sm:left-[-25%] md:!left-[44%]' : ''}`}>
+                                <span ref={el => linesRef.current[index] = el} className={`absolute rotate-90 ${index % 2 === 0 ? ' md:rotate-0' : 'md:rotate-180'} z-10 -top-1 width-[6px] height-[35px] xxs:height-[55px] ${index % 2 === 0 ? 'md:top-[-100%]' : 'md:top-[100%]'} max-md:${index % 2 === 0 ? '-rotate-90' : 'rotate-90'} left-[80%] xxs:left-[120%] md:left-[44%] animate-lineMove ${index === 0 || index === 2 || index === 4 ? 'sm:-rotate-90 sm:left-[-25%] md:!left-[44%]' : ''} ${index === 1 || index === 3 || index === 5 ? 'rotate-180' : ''}`}>
                                     <BlurLine />
                                 </span>
                             </div>
                         ))}
+
                         <hr ref={hrRef} className='absolute top-[2%] border-t-0 md:[2px] h-[2px] md:top-7 w-[2px] left-[21px] sm:left-[20px] md:left-[25px] md:w-full !bg-[#313543] max-sm:min-h-[870px] max-md:min-h-[461px] max-sm:h-[97%] md:max-w-[700px] lg:max-w-[895px] xl:max-w-[880px] lg:w-full' />
                     </div>
                     <div className="flex flex-col md:flex-row gap-6 lg:gap-12 items-end md:items-center justify-end">
                         {steps.slice(3).map((step, index) => (
                             <Customcard
                                 key={index + 3}
-                                ref={el => cardsRef.current[index + 3] = el}
                                 image={step.image}
                                 content={step.content}
                                 className={`animate-shadowMove delay-${index * 300}`}
